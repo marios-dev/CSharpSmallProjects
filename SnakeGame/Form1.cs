cc8a033d872c95cc73c4aff3996a7679e8bb42ab
Mario Syti <msyti@outlook.com>
2021-10-19 23:44:38 +0300

    snake game

---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        private List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Set settings to default
+            new Settings();
+
+            //Set game speed and start timer
+            gameTimer.Interval = 1000 / Settings.Speed;
+            gameTimer.Tick += UpdateScreen();
+            gameTimer.Start();
+
+            // StartNew game
+            StartGame();
+        }
+        private void StartGame()
+        {
+            //Set settings to defaut
+            new Settings();
+
+            //Create new player object
+            Snake.Clear();
+            Circle head = new Circle();
+            head.X = 10;
+            head.Y = 5;
+            Snake.Add(head);
+
+            lblScore.Text = Settings.Score.ToString();
+            GenerateFood();
+
+        }
+        //place random food game
+        private void GenerateFood()
+        {
+            int maxXPos = pbCanvas.Size.Width / Settings.Width;
+            int maxYPos = pbCanvas.Size.Height / Settings.Height;
+
+            Random random = new Random();
+            food = new Circle();
+            food.X = random.Next(0,maxXPos);
+            food.Y=random.Next(0, )
+        }
+    }
+}
